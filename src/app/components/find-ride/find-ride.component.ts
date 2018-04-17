4b16c20456b780009fc8751b96340fa4975bed49
Vladyslav Chernenko <Vladyslav_Chernenko@epam.com>
2018-04-17 14:25:01 +0300

    add images

---
+++ src/app/components/find-ride/find-ride.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-ride',
+  templateUrl: './find-ride.component.html',
+  styleUrls: ['./find-ride.component.scss']
+})
+export class FindRideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
