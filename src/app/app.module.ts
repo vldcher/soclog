a8fc4f442a05b93e731b6c11a8f39b448be3bdea
Vladyslav Chernenko <Vladyslav_Chernenko@epam.com>
2018-04-20 14:25:37 +0300

    finished form markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
--- src/app/components/offer-details/offer-description/offer-description.component.ts
+++ src/app/components/offer-details/offer-description/offer-description.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferDescriptionComponent implements OnInit {
 
-  model;
   constructor() { }
 
   ngOnInit() {
